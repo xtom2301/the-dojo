47b905b64587f14d96079cc86cd8fd9c22a09119
tmartin <xtom2301@gmail.com>
2022-02-11 20:33:26 +0100

    reuse hooks and context

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAQvMLKVf2q_8eVO3JNc81f8ZXrr4QcgxM',
+  authDomain: 'dojo-site-9594e.firebaseapp.com',
+  projectId: 'dojo-site-9594e',
+  storageBucket: 'dojo-site-9594e.appspot.com',
+  messagingSenderId: '920843005118',
+  appId: '1:920843005118:web:a6f6e726d23b4b1ca702ef',
+  measurementId: 'G-47SMVT0DH1',
+};
+
+firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
