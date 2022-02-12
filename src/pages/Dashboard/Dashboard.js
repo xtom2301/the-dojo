97f0814b75c289c8b52875560d2ec0096d6bf1c7
tmartin <xtom2301@gmail.com>
2022-02-12 20:52:59 +0100

    show all projects on dashboard site

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,18 @@
 import './Dashboard.css';
 
+import { useCollection } from '../../hooks/useCollection';
+import ProjectList from '../../components/ProjectList';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection('projects');
+
+  return (
+    <div>
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 };
 
 export default Dashboard;
